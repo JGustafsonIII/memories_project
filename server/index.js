b7974ac9e0b03d008fe29bb92d6e266bf19cfdb1
stillmoon <jgustafson1997@hotmail.com>
2021-04-09 16:21:07 -0400

    Set up basic express server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,34 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://gustafson:N8waT9t0QLmfPoZi@cluster0.w2imm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+/**
+ * Used to connect to the database and start listening to the express server at port 5000
+ */
+const connect = async () => {
+  const connection = await mongoose
+    .connect(CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((error) => console.log(error));
+
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  mongoose.set('useFindAndModify', false);
+};
+
+connect();
