c63d02a64e8c5e234299de158ad59116725b5ab0
stillmoon <jgustafson1997@hotmail.com>
2021-04-11 12:48:25 -0400

    Backend functionality for deleting a post complete

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,7 +7,7 @@ export const getPosts = async (req, res) => {
     const postMessages = await PostMessage.find();
     res.status(200).json(postMessages);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({ message: error });
   }
 };
 
@@ -25,7 +25,7 @@ export const createPost = async (req, res) => {
     await newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    res.status(409).json({ message: error });
   }
 };
 
@@ -44,6 +44,21 @@ export const updatePost = async (req, res) => {
     });
     res.json(updatedPost);
   } catch (error) {
-    console.log(error.message);
+    res.status(409).json({ message: error });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send(`No post with id: ${id}`);
+  }
+
+  try {
+    await PostMessage.findByIdAndRemove(id);
+    res.json({ message: 'Post Deleted Successfully' });
+  } catch (error) {
+    res.status(404).json({ message: error });
   }
 };
